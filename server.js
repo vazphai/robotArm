07e1cc4f963f15a788e95e11add15e8b9eb15a72
unknown <hatekuu@gmail.com>
2025-03-02 15:50:36 +0700

    dong123123

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ connectDB().then(() => {
     if (!data&&!id)
       return res.status(401).status("khong co truong data va id")
     try {
-      const result = await controlCollection.updateOne({_id: new ObjectId(id)},{$set:{val}});
+      const result = await controlCollection.updateOne({_id: id},{$set:{val}});
       res.status(201).json(result);
     } catch (err) {
       res.status(400).json({ message: err.message });
